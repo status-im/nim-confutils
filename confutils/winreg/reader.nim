4304251b09ea88341409433d8210c911bca83fc2
jangko <jangko128@gmail.com>
2020-10-21 16:07:25 +0700

    winreg encoder-decoder implementation

--- confutils/winreg/reader.nim
+++ confutils/winreg/reader.nim
@@ -7,6 +7,7 @@ type
   WinregReader* = object
     hKey: HKEY
     path: string
+    key: seq[string]
 
   WinregReaderError* = object of WinregError
 
@@ -29,23 +30,54 @@ proc init*(T: type WinregReader,
   result.hKey = hKey
   result.path = path
 
+template getUnderlyingType*[T](_: Option[T]): untyped = T
+
 proc readValue*[T](r: var WinregReader, value: var T)
                   {.raises: [SerializationError, IOError, Defect].} =
   mixin readValue
+  # TODO: reduce allocation
 
-  when T is (SomePrimitives or range):
-    getValue(w.hKey, w.path, w.key, value)
+  when T is (SomePrimitives or range or string):
+    let path = constructPath(r.path, r.key)
+    discard getValue(r.hKey, path, r.key[^1], value)
+  elif T is Option:
+    let path = constructPath(r.path, r.key)
+    var outVal: getUnderlyingType(value)
+    if getValue(r.hKey, path, r.key[^1], outVal):
+      value = some(outVal)
   elif T is (seq or array):
     when uTypeIsPrimitives(T):
-      getValue(w.hKey, w.path, w.key, value)
+      let path = constructPath(r.path, r.key)
+      discard getValue(r.hKey, path, r.key[^1], value)
     elif uTypeIsRecord(T):
-      # TODO: reduce allocation
-      discard
+      let key = r.key[^1]
+      for i in 0..<value.len:
+        r.key[^1] = key & $i
+        r.readValue(value[i])
     else:
       const typeName = typetraits.name(T)
       {.fatal: "Failed to convert from Winreg array an unsupported type: " & typeName.}
   elif T is (object or tuple):
-    discard
+    type T = type(value)
+
+    when T.totalSerializedFields > 0:
+      let fields = T.fieldReadersTable(WinregReader)
+      var expectedFieldPos = 0
+      r.key.add ""
+      value.enumInstanceSerializedFields(fieldName, field):
+        type FieldType = type field
+
+        when T is tuple:
+          r.key[^1] = $expectedFieldPos
+          var reader = fields[][expectedFieldPos].reader
+          expectedFieldPos += 1
+        else:
+          r.key[^1] = fieldName
+          var reader = findFieldReader(fields[], fieldName, expectedFieldPos)
+
+        if reader != nil:
+          reader(value, r)
+      discard r.key.pop()
   else:
     const typeName = typetraits.name(T)
     {.fatal: "Failed to convert from Winreg an unsupported type: " & typeName.}
--- confutils/winreg/writer.nim
+++ confutils/winreg/writer.nim
@@ -7,7 +7,7 @@ type
   WinregWriter* = object
     hKey: HKEY
     path: string
-    key: string
+    key: seq[string]
 
 proc init*(T: type WinregWriter,
            hKey: HKEY, path: string): T =
@@ -16,20 +16,34 @@ proc init*(T: type WinregWriter,
 
 proc writeValue*(w: var WinregWriter, value: auto) =
   mixin enumInstanceSerializedFields, writeValue, writeFieldIMPL
+  # TODO: reduce allocation
 
-  when value is (SomePrimitives or range):
-    setValue(w.hKey, w.path, w.key, value)
+  when value is (SomePrimitives or range or string):
+    let path = constructPath(w.path, w.key)
+    discard setValue(w.hKey, path, w.key[^1], value)
+  elif value is Option:
+    if value.isSome:
+      w.writeValue value.get
   elif value is (seq or array or openArray):
     when uTypeIsPrimitives(type value):
-      setValue(w.hKey, w.path, w.key, value)
+      let path = constructPath(w.path, w.key)
+      discard setValue(w.hKey, path, w.key[^1], value)
     elif uTypeIsRecord(type value):
-      # TODO: reduce allocation
-      discard
+      let key = w.key[^1]
+      for i in 0..<value.len:
+        w.key[^1] = key & $i
+        w.writeValue(value[i])
     else:
       const typeName = typetraits.name(value.type)
       {.fatal: "Failed to convert to Winreg array an unsupported type: " & typeName.}
   elif value is (object or tuple):
-    discard
+    type RecordType = type value
+    w.key.add ""
+    value.enumInstanceSerializedFields(fieldName, field):
+      type FieldType = type field
+      w.key[^1] = fieldName
+      w.writeFieldIMPL(FieldTag[RecordType, fieldName, FieldType], field, value)
+    discard w.key.pop()
   else:
     const typeName = typetraits.name(value.type)
     {.fatal: "Failed to convert to Winreg an unsupported type: " & typeName.}
