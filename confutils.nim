73403597029a6de407a0d20eb7d11b95bdd0627d
jangko <jangko128@gmail.com>
2024-02-12 10:26:05 +0700

    Add copyright to source code

--- confutils.nim
+++ confutils.nim
@@ -1,3 +1,12 @@
+# confutils
+# Copyright (c) 2018-2024 Status Research & Development GmbH
+# Licensed under either of
+#  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE))
+#  * MIT license ([LICENSE-MIT](LICENSE-MIT))
+# at your option.
+# This file may not be copied, modified, or distributed except according to
+# those terms.
+
 import
   os,
   std/[enumutils, options, strutils, wordwrap, strformat],
@@ -889,7 +898,7 @@ func constructEnvKey*(prefix: string, key: string): string {.raises: [].} =
 # See https://nim-lang.org/docs/os.html#commandLineParams
 when not declared(commandLineParams):
   proc commandLineParams(): seq[string] = discard
- 
+
 proc loadImpl[C, SecondarySources](
     Configuration: typedesc[C],
     cmdLine = commandLineParams(),
--- confutils/cli_parser.nim
+++ confutils/cli_parser.nim
@@ -1,4 +1,12 @@
-# Copyright 2018-2022 Status Research & Development GmbH
+# confutils
+# Copyright (c) 2018-2024 Status Research & Development GmbH
+# Licensed under either of
+#  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE))
+#  * MIT license ([LICENSE-MIT](LICENSE-MIT))
+# at your option.
+# This file may not be copied, modified, or distributed except according to
+# those terms.
+
 # Parts taken from Nim's Runtime Library (c) Copyright 2015 Andreas Rumpf
 
 type
