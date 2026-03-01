b0554bb386b9fd74c0c090838dd4cfb3dbb270c1
nitely <ecastroborsani@gmail.com>
2026-03-01 01:56:50 -0300

    tests

--- tests/help/test_default_value_desc.nim
+++ tests/help/test_default_value_desc.nim
@@ -7,17 +7,18 @@
 # This file may not be copied, modified, or distributed except according to
 # those terms.
 
+import os
 import ../../confutils
 
 const defaultEth2TcpPort = 9000
-const defaultDescOverride = "overridden"
+const defaultDescOverride = "ok"
 
 type DisString = distinct string
 type NoDollar = distinct string
 
 proc `$`(s: DisString): string =
   # this should show in the help message
-  "foobar"
+  "ok"
 
 proc completeCmdArg(T: type DisString, val: string): seq[string] =
   completeCmdArg(string, val)
@@ -31,6 +32,18 @@ proc completeCmdArg(T: type NoDollar, val: string): seq[string] =
 proc parseCmdArg(T: type NoDollar, s: string): T =
   parseCmdArg(string, s).NoDollar
 
+proc runtimeVal(): string =
+  # do something that's not available at comptime
+  {.cast(raises: []).}:
+    discard getCurrentProcessId()
+  "ok"
+
+proc canRaise(): string {.raises: [ValueError].} =
+  # do something that's not available at comptime
+  if false:
+    raise (ref ValueError)(msg: "never raised")
+  "bad"
+
 type
   TestConf = object
     opt1 {.
@@ -41,24 +54,23 @@ type
 
     opt2 {.
       defaultValue: defaultEth2TcpPort
-      #defaultValueDesc: $defaultEth2TcpPort
       desc: "tcp port 2"
       name: "opt2" }: int
 
     opt3 {.
-      defaultValue: defaultEth2TcpPort
+      defaultValue: "bad"
       defaultValueDesc: defaultDescOverride
-      desc: "tcp port 3"
-      name: "opt3" }: int
+      desc: "const default value desc"
+      name: "opt3" }: string
 
     opt4 {.
-      defaultValue: defaultEth2TcpPort
-      defaultValueDesc: "overridden"
-      desc: "tcp port 4"
-      name: "opt4" }: int
+      defaultValue: "bad"
+      defaultValueDesc: "ok"
+      desc: "literal default value desc"
+      name: "opt4" }: string
 
     opt5 {.
-      defaultValue: DisString("this should not show in the help message")
+      defaultValue: DisString("bad")
       desc: "distinct string"
       name: "opt5" }: DisString
 
@@ -68,9 +80,43 @@ type
       name: "opt6" }: NoDollar
 
     opt7 {.
-      defaultValue: NoDollar("default")
-      defaultValueDesc: "overridden"
-      desc: "no dollar func defined"
+      defaultValue: NoDollar("bad")
+      defaultValueDesc: "ok"
+      desc: "no dollar func defined with default desc"
       name: "opt7" }: NoDollar
+
+    opt8 {.
+      defaultValue: runtimeVal()
+      desc: "runtime value"
+      name: "opt8" }: string
+
+    opt9 {.
+      defaultValue: "bad"
+      defaultValueDesc: runtimeVal()
+      desc: "runtime value"
+      name: "opt9" }: string
+
+    opt10 {.
+      defaultValue: "bad"
+      defaultValueDesc: runtimeVal() & " concat"
+      desc: "runtime value"
+      name: "opt10" }: string
+
+    opt11 {.
+      defaultValue: canRaise()
+      defaultValueDesc: "ok"
+      desc: "default value can raise but default desc won't"
+      name: "opt11" }: string
+
+    opt12 {.
+      defaultValue: config.opt11
+      desc: "default is config.opt11"
+      name: "opt12" }: string
+
+    opt13 {.
+      defaultValue: config.opt11
+      defaultValueDesc: "ok"
+      desc: "default is config.opt11 with default desc"
+      name: "opt13" }: string
 
 let c = TestConf.load(termWidth = int.high)
